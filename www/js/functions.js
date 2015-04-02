bb71c4f9d7b2f9c19845bd40529ae0f12d64824b
M.Ulyanov <m.ulyanov@roscontrol.com>
2015-04-02 18:48:33 +0300

    update

--- www/js/functions.js
+++ www/js/functions.js
@@ -8,18 +8,22 @@ app.controller('funcController', function($scope) {
 		//console.log(rating, tested, dangerLevel)
 
   	if(tested == 0) {
+  		console.log(1);
   		$scope.rating = '?';
   		return 'product__rating-wait';
   	}
   	else if(tested && (dangerLevel >= 2)) {
+  		console.log(2);
   		$scope.rating = 'X';
   		return 'product__rating-black';
   	}
   	else if(tested && (dangerLevel == 1)) {
+  		console.log(3);
   		$scope.rating = rating;
   		return 'product__rating-violation';
   	}
   	else {
+  		console.log(4);
   		$scope.rating = rating;
   		return 'product__rating-green';
   	}
