82e0a0f5913f02fe0a34f4f766f73c4fb8ae9d1d
Vladimir Voronin <vl@roscontrol.com>
2015-04-13 13:07:24 +0300

    add fabric User

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -198,7 +198,7 @@ app.controller('MenuCtrl', function($scope) {
  
 
 // Контроллер авторизации
-app.controller('AuthorizationCtrl', function($scope, $http, $ionicModal, Authorization) {
+app.controller('AuthorizationCtrl', function($scope, $http, $ionicModal, User) {
   	
   // Шаблон
   $ionicModal.fromTemplateUrl('templates/login.html', {
@@ -225,12 +225,9 @@ app.controller('AuthorizationCtrl', function($scope, $http, $ionicModal, Authori
 
   	$scope.loginResult = '';
 		
-		Authorization.login($scope.loginData).then(function(result) {
+		User.login($scope.loginData.username, $scope.loginData.password).then(function(profile) {
 			//window.localStorage.clear();
-	  	if(result.status == 200) {
-	  		$scope.profile = result.data.profile;
-	  		localStorage.setItem('pk_userprofile', JSON.stringify($scope.profile));
-	  	}
+	  		$scope.profile = profile;
 		}, 
 		function(err) {
 	  	if(err.status == 403) {
@@ -245,14 +242,13 @@ app.controller('AuthorizationCtrl', function($scope, $http, $ionicModal, Authori
 
   // Разлогиниться
   $scope.logout = function() {
-  	localStorage.removeItem('pk_userprofile');
+  	User.logout();
   }
 
   // Проверка авторизации
   $scope.checkAuthorization = function() {
-  	var retrievedObject = localStorage.getItem('pk_userprofile');
-  	$scope.userProfile = retrievedObject;
-  	return retrievedObject ? true : false; 
+  	$scope.userProfile = User.profile();
+  	return User.is_auth(); 
   }
 
 });
---
+++ www/js/user-services.js
@@ -0,0 +1,52 @@
+angular.module('user-services', [])
+
+// Resource service example
+.factory('User', function($http) {
+    var self = this;
+    var user_key = 'rk_user';
+    var user = null;
+
+  	self.login = function(email, password) {
+        self.logout();
+	  	return $http.get('/v1/auth/email?' + 'email=' + email + '&password=' + password).
+	  		then(function(result) {
+                console.log("user", result.data);
+
+                if (result.status == 200) {
+                    user = result.data;
+                    localStorage.setItem(user_key, JSON.stringify(user));
+                    return user.profile;
+                }
+
+	    		return null;
+	  	    }, function(status) {
+		        return status;
+		    });
+  	};
+
+  	self.logout = function() {
+        localStorage.removeItem(user_key);
+        user = null;
+  	};
+
+    self.profile = function() {
+        if (user) {
+            return user.profile;
+        }
+
+        var uj = localStorage.getItem(user_key);
+
+        if (!uj) {
+            return null;
+        }
+
+        user = JSON.parse(uj);
+        return user.profile;
+    };
+
+    self.is_auth = function() {
+        return user ? true : false;
+    };
+
+    return self;
+});
