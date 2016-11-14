ef7141c2bf9856a6da09e0a3cdb94b9cbf5b5b00
maxim <web.ulyanov@gmail.com>
2016-11-14 17:18:46 +0300

    Рефакторинг. Почистил app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
-import { Component, ViewChild} from '@angular/core';
-
-import { Events, MenuController, Nav, Platform } from 'ionic-angular';
+import { Component} from '@angular/core';
+import { Platform } from 'ionic-angular';
 import { Splashscreen, StatusBar } from 'ionic-native';
 
-import { AccountPage } from '../pages/account/account';
-import { LoginPage } from '../pages/login/login';
-import { SignupPage } from '../pages/signup/signup';
 import { TabsPage } from '../pages/tabs/tabs';
 import { TutorialPage } from '../pages/tutorial/tutorial';
 
@@ -14,14 +10,6 @@ import { DbService } from '../services/db.service';
 import { JournalService } from '../services/journal.service';
 
 
-export interface PageObj {
-    title: string;
-    component: any;
-    icon: string;
-    logsOut?: boolean;
-    index?: number;
-}
-
 
 @Component({
     templateUrl: 'app.template.html',
@@ -29,44 +17,26 @@ export interface PageObj {
 
 
 export class App {
-    // the root nav is a child of the root app component
-    // @ViewChild(Nav) gets a reference to the app's root nav
-    @ViewChild(Nav) nav:Nav;
 
-    // List of pages that can be navigated to from the left menu
-    // the left menu only works after login
-    // the login page disables the left menu
-    appPages:PageObj[] = [
-        {title: 'Schedule', component: TabsPage, icon: 'calendar'},
-        {title: 'Speakers', component: TabsPage, index: 1, icon: 'contacts'},
-        {title: 'Map', component: TabsPage, index: 2, icon: 'map'},
-        {title: 'About', component: TabsPage, index: 3, icon: 'information-circle'},
-    ];
-    loggedInPages:PageObj[] = [
-        {title: 'Account', component: AccountPage, icon: 'person'},
-        {title: 'Logout', component: TabsPage, icon: 'log-out', logsOut: true}
-    ];
-    loggedOutPages:PageObj[] = [
-        {title: 'Login', component: LoginPage, icon: 'log-in'},
-        {title: 'Signup', component: SignupPage, icon: 'person-add'}
-    ];
 
-    rootPage:any = null;
-    private prevLaunched:any = null;
+    private rootPage: any;
+    private prevLaunched: any;
 
-    constructor(public events:Events,
-                public menu:MenuController,
-                platform:Platform,
-                private dbService:DbService,
-                private journalService: JournalService
-                ) {
+
+    /**
+     *
+     * @param platform
+     * @param dbService
+     * @param journalService
+     */
+    constructor(private platform:Platform, private dbService:DbService, private journalService: JournalService) {
+
         // Call any initial plugins when ready
         platform.ready().then(() => {
             StatusBar.overlaysWebView(true); // let status bar overlay webview
             StatusBar.backgroundColorByHexString('#a32630'); // set status bar to white
             Splashscreen.hide();
         });
-
 
         // Use TutorialPage for fist launch app
         this.prevLaunched = LocalStorage.get('prevLaunchApp');
@@ -77,53 +47,13 @@ export class App {
         else {
             this.rootPage = TabsPage;
         }
-
         this.prepareData();
-        this.listenToLoginEvents();
-    }
-
-    openPage(page:PageObj) {
-        // the nav component was found using @ViewChild(Nav)
-        // reset the nav to remove previous pages and only have this page
-        // we wouldn't want the back button to show in this scenario
-
-
-        if (page.index) {
-            this.nav.setRoot(page.component, {tabIndex: page.index});
-        }
-        else {
-            this.nav.setRoot(page.component);
-        }
-
-        if (page.logsOut === true) {
-            // Give the menu time to close before changing to logged out
-            setTimeout(() => {
-            }, 1000);
-        }
-    }
-
-    listenToLoginEvents() {
-        this.events.subscribe('user:login', () => {
-            this.enableMenu(true);
-        });
-
-        this.events.subscribe('user:signup', () => {
-            this.enableMenu(true);
-        });
-
-        this.events.subscribe('user:logout', () => {
-            this.enableMenu(false);
-        });
-    }
-
-    enableMenu(loggedIn) {
-        this.menu.enable(loggedIn, 'loggedInMenu');
-        this.menu.enable(!loggedIn, 'loggedOutMenu');
     }
 
 
-
-
+    /**
+     *
+     */
     private prepareData(): void {
 
         //
